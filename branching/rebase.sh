32ba61844fcf9619c2d9132a92c00b3b690b5049
Pavel <kazakov.pm@gmail.com>
2022-04-09 21:10:24 +0700

    accept all changes (force)

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,14 +3,10 @@
 
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
 
-    echo "\$@ Parameter #$count = $param"
-    echo "Parameter: $param"
-=======
     echo "Next parameter: $param"
     count=$(( $count + 1 ))
->>>>>>> d623a12 (git rebase 2)
+
 done
 
 echo "====="
